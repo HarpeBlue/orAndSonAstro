aa6195f9b66e4ff2cdce88dc9f0ff0001e4a2f7b
HarpeBlue <hspe2507@gmail.com>
2022-10-09 22:53:39 -0500

    :sparkles: Finish customize

--- theme/src/config/settings.js
+++ theme/src/config/settings.js
@@ -1,7 +1,7 @@
 export default {
-	title: `Odyssey Astro Theme | A Marketing Website Theme for Startups and Businesses`,
-	description: `A simple, clean, and modern theme for a startup or businesses' marketing website.`,
-	url: `https://odyssey-theme.littlesticks.dev`, // No trailing slash!
+	title: `Or & Son construction LLC `,
+	description: `construction site`,
+	url: `https://orsoncllc.com/`, // No trailing slash!
 	name: `Or & Son Construction LLC`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
 	showLittleSticksPlug: false, // Disable this if you want to remove the Little Sticks plug from the footer. (╯°□°)╯︵ ┻━┻
